ceba2b2e6e2ce6a767ca265034c35e1299ebbc54
Asad Khaksar <119926803+asadkhaksar1122@users.noreply.github.com>
2025-02-27 22:05:18 -0800

    have done some fixes

--- src/app/my-component/todos/todos.component.ts
+++ src/app/my-component/todos/todos.component.ts
@@ -16,7 +16,11 @@ export class TodosComponent {
     localStorage.getItem('angulartodos') || '[]'
   );
   alertshow: AlertType | null = null;
+  status: 'All' | 'completed' | 'Notcompleted' = 'All';
   deletetask(id: string) {
+    if (this.status !== 'All') {
+      this.Alltodo = JSON.parse(localStorage.getItem('angulartodos') || '[]');
+    }
     this.Alltodo = this.Alltodo.filter((task) => task.id !== id);
     localStorage.setItem('angulartodos', JSON.stringify(this.Alltodo));
     this.alertshow = {
@@ -26,9 +30,15 @@ export class TodosComponent {
     setTimeout(() => {
       this.alertshow = null;
     }, 2000);
+    if (this.status !== 'All') {
+      this.filterout(this.status);
+    }
   }
 
   taskcompleted(id: string) {
+    if (this.status !== 'All') {
+      this.Alltodo = JSON.parse(localStorage.getItem('angulartodos') || '[]');
+    }
     this.Alltodo = this.Alltodo.map((task) =>
       task.id === id ? { ...task, completed: !task.completed } : task
     );
@@ -40,9 +50,15 @@ export class TodosComponent {
     setTimeout(() => {
       this.alertshow = null;
     }, 2000);
+    if (this.status !== 'All') {
+      this.filterout(this.status);
+    }
   }
 
   taskadded(newtodo: TodoType) {
+    if (this.status !== 'All') {
+      this.Alltodo = JSON.parse(localStorage.getItem('angulartodos') || '[]');
+    }
     this.Alltodo.unshift(newtodo);
     localStorage.setItem('angulartodos', JSON.stringify(this.Alltodo));
     this.alertshow = {
@@ -52,17 +68,23 @@ export class TodosComponent {
     setTimeout(() => {
       this.alertshow = null;
     }, 2000);
+    if (this.status != 'All') {
+      this.filterout(this.status);
+    }
   }
 
   filterout(filter: 'completed' | 'Notcompleted' | 'All') {
     if (filter === 'completed') {
       this.Alltodo = JSON.parse(localStorage.getItem('angulartodos') || '[]');
       this.Alltodo = this.Alltodo.filter((task) => task.completed === true);
+      this.status = 'completed';
     } else if (filter === 'Notcompleted') {
       this.Alltodo = JSON.parse(localStorage.getItem('angulartodos') || '[]');
       this.Alltodo = this.Alltodo.filter((task) => task.completed === false);
+      this.status = 'Notcompleted';
     } else if (filter === 'All') {
       this.Alltodo = JSON.parse(localStorage.getItem('angulartodos') || '[]');
+      this.status = 'All';
     }
     console.log('console from parent');
   }
